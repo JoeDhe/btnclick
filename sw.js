67a3b7e7075d7a6d77e3802f03196e360fb32fb3
JoeDhe <95077245+JoeDhe@users.noreply.github.com>
2021-12-04 16:27:33 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,78 +1,26 @@
-const version = 6;
-const preCacheName = `static-${version}`;
-const preCache = ['/', '/index.html', '/404.html'];
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js'
+];
 
-self.addEventListener('install', (ev) => {
-  //installed
-  ev.waitUntil(
-    caches
-      .open(preCacheName)
-      .then((cache) => {
-        console.log('caching the static files');
-        cache.addAll(preCache);
-      })
-      .catch(console.warn)
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
   );
-  //load pre-cache
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (ev) => {
-  //activating
-  ev.waitUntil(
-    caches
-      .keys()
-      .then((keys) => {
-        return Promise.all(
-          keys
-            .filter((key) => key !== preCacheName)
-            .map((key) => caches.delete(key))
-        );
-      })
-      .catch(console.warn)
-  );
-  //delete old caches
-});
-
-self.addEventListener('fetch', (ev) => {
-  //fetch request received
-  //send back a response from cache or fetch
-  ev.respondWith(
-    caches.match(ev.request).then((cacheRes) => {
-      return (
-        cacheRes ||
-        fetch(ev.request).then(
-          (response) => {
-            return response;
-          },
-          (err) => {
-            //network failure
-            //send something else from the cache?
-            if (
-              ev.request.url.indexOf('.html') > -1 ||
-              ev.request.mode == 'navigation'
-            ) {
-              return caches.match('/404.html');
-            }
-          }
-        )
-      );
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
-
-self.addEventListener('message', (ev) => {
-  //message received
-  //do things based on message props
-  let data = ev.data;
-  console.log('SW received', data);
-});
-
-const sendMessage = async (msg) => {
-  let allClients = await clients.matchAll({ includeUncontrolled: true });
-  return Promise.all(
-    allClients.map((client) => {
-      let channel = new MessageChannel();
-      return client.postMessage(msg);
-    })
-  );
-};
