b400ca80a19e98c082075eb52236515620c4caba
JoeDhe <95077245+JoeDhe@users.noreply.github.com>
2021-12-05 22:05:01 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,6 @@ var cacheName = 'hello-pwa';
 var filesToCache = [
   '/',
   '/index.html',
-  '/css/style.css',
-  '/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
