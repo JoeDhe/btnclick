32845e3d27d776512fa1fbdb09a8fd39b7fc16c2
JoeDhe <95077245+JoeDhe@users.noreply.github.com>
2021-12-06 17:08:49 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,62 @@
-var cacheName = 'hello-pwa';
-var filesToCache = [
-  '/',
-  '/index.html',
+self.addEventListener('install', function(e) {
+    console.log('[Service Worker] Install');
+});
+
+var cacheName = 'v1';
+var appShellFiles = [
+    './',
+    './index.html',    
 ];
 
-/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(appShellFiles);
+        })
+    );
   self.skipWaiting();
 });
 
-
-/* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        caches.match(e.request).then(function(r) {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+            return r || fetch(e.request).then(function(response) {
+                return caches.open(cacheName).then(function(cache) {
+                    console.log('[Service Worker] Caching new resource: '+e.request.url);
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
 });
+
+
+// var cacheName = 'hello-pwa';
+// var filesToCache = [
+//   '/',
+//   '/index.html',
+// ];
+
+// /* Start the service worker and cache all of the app's content */
+// self.addEventListener('install', function(e) {
+//   e.waitUntil(
+//     caches.open(cacheName).then(function(cache) {
+//       return cache.addAll(filesToCache);
+//     })
+//   );
+//   self.skipWaiting();
+// });
+
+
+// /* Serve cached content when offline */
+// self.addEventListener('fetch', function(e) {
+//   e.respondWith(
+//     caches.match(e.request).then(function(response) {
+//       return response || fetch(e.request);
+//     })
+//   );
+// });
