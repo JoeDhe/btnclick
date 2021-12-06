e44b9d883ee96a2788027fdb549a8ff13739dbd1
JoeDhe <95077245+JoeDhe@users.noreply.github.com>
2021-12-06 21:37:44 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,62 +1,32 @@
-self.addEventListener('install', function(e) {
-    console.log('[Service Worker] Install');
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
+  event.waitUntil(
+    caches.open('static')
+      .then(function (cache) {
+        // cache.add('/');
+        // cache.add('/index.html');
+        // cache.add('/src/js/app.js');
+        cache.addAll([
+          '/',
+          '/index.html',
+        ]);
+      })
+  );
 });
 
-var cacheName = 'v1';
-var appShellFiles = [
-    './',
-    './index.html',    
-];
-
-self.addEventListener('install', function(e) {
-    console.log('[Service Worker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            console.log('[Service Worker] Caching all: app shell and content');
-            return cache.addAll(appShellFiles);
-        })
-    );
-  self.skipWaiting();
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
 });
 
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(r) {
-            console.log('[Service Worker] Fetching resource: '+e.request.url);
-            return r || fetch(e.request).then(function(response) {
-                return caches.open(cacheName).then(function(cache) {
-                    console.log('[Service Worker] Caching new resource: '+e.request.url);
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
-        })
-    );
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
 });
-
-
-// var cacheName = 'hello-pwa';
-// var filesToCache = [
-//   '/',
-//   '/index.html',
-// ];
-
-// /* Start the service worker and cache all of the app's content */
-// self.addEventListener('install', function(e) {
-//   e.waitUntil(
-//     caches.open(cacheName).then(function(cache) {
-//       return cache.addAll(filesToCache);
-//     })
-//   );
-//   self.skipWaiting();
-// });
-
-
-// /* Serve cached content when offline */
-// self.addEventListener('fetch', function(e) {
-//   e.respondWith(
-//     caches.match(e.request).then(function(response) {
-//       return response || fetch(e.request);
-//     })
-//   );
-// });
