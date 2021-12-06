be629e9566956bd8a7567df87d5231db9eb4026f
JoeDhe <95077245+JoeDhe@users.noreply.github.com>
2021-12-06 16:06:10 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,9 +14,6 @@ self.addEventListener('install', function(e) {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-   console.log("Service worker activated");
-});
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
