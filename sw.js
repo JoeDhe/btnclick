ca0981a4bf125a092dad0f257b886636396a6e13
maxsoftmedia <95077245+JoeDhe@users.noreply.github.com>
2021-12-11 00:59:55 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,23 @@
+const static = 'static-site';
+const assets = [
+//                 './',
+//                 './index.html',
+//                 './apps.html',
+//                 'https://script.google.com/macros/s/AKfycbyI2upLldJVAirdKywq5siMVJp2j55_gFJTv5cXTIKOcUu8hdCcYbBd2G5ciaHODzIRFA/exec',
+//                 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css',
+//                 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
+                  './offline.html',
+              ];
+
 self.addEventListener('install', function (event) {
   console.log('SW Installed');
-  event.waitUntil(
-    caches.open('static')
-      .then(function (cache) {
-        // cache.add('/');
-        // cache.add('/index.html');
-        // cache.add('/src/js/app.js');
-        cache.addAll([
-          './',
-          './index.html',
-          './apps.html',
-          'https://script.google.com/macros/s/AKfycbyI2upLldJVAirdKywq5siMVJp2j55_gFJTv5cXTIKOcUu8hdCcYbBd2G5ciaHODzIRFA/exec',
-          'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css',
-          'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
-        ]);
-      })
-  );
-});
+    event.waitUntil(
+      caches.open('static')
+        .then(function (cache) {
+          cache.addAll(assets);
+        })
+    );
+  });
 
 self.addEventListener('activate', function () {
   console.log('SW Activated');
